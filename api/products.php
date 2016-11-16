5a053d7ee7002eefad3ab765341e0ed8a396e1cc
Jeff Puckett <jeff@jeffpuckett.com>
2016-11-16 03:29:45 +0000

    refactor, validate, and document

--- api/products.php
+++ api/products.php
@@ -1,23 +1,35 @@
 <?php
 
-if(isset($_GET['id'])){
-  $pid = $_GET['id'];
+// get database connection
+$db = require __DIR__."/../odbc.pdo.php";
+
+$sql = "SELECT * FROM [ISYS4283].[dbo].[products]";
+
+// if URL query string
+// has a product ID filter
+// and validate user input is an integer
+if(isset($_GET['id']) && is_int(filter_var($_GET['id'], FILTER_VALIDATE_INT))){
+
+	// append SQL with parameterized placeholder
+	$sql .= " WHERE id = :id_parameter";
+
+	// prepare the statement
+	$statement = $db->prepare($sql);
+
+	// execute the statement
+	// with user submitted data
+	$statement->execute([
+		"id_parameter" => $_GET['id'],
+	]);
+}
+// else just query the SELECT statement
+// without a WHERE clause
+else {
+	$statement = $db->query($sql);
 }
 
-$pdo = require __DIR__."/../odbc.pdo.php";
+// fetch all rows with associative (non-numeric) indexes
+$result = $statement->fetchAll(PDO::FETCH_ASSOC);
 
-$sql = "SELECT * FROM [ISYS4283].[dbo].[products] WHERE id = :id";
-
-$smt = $pdo->prepare($sql);
-
-$smt->execute([
-  "id" => $pid,
-]);
-
-$res = $smt->fetchAll();
-
-//$res = $pdo->query($sql)->fetchAll();
-
-$json = json_encode($res, JSON_PRETTY_PRINT);
-
-echo $json;
+// print out all data in JSON format
+echo json_encode($result, JSON_PRETTY_PRINT);
