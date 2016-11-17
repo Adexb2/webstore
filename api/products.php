dba76a1ca928e6570d840b4e134dd9fb048d0e68
Jeff Puckett <jeff@jeffpuckett.com>
2016-11-17 15:59:40 +0000

    encapsulate database call for products array

--- api/products.php
+++ api/products.php
@@ -1,59 +1,6 @@
 <?php
 
-// get database connection
-$db = require __DIR__."/../pdo.php";
-
-$sql = "
-	-- if it gets any more complex than this, then should use a view
-	SELECT p.id, p.name, p.description, p.image, p.price,
-		p.vendor, v.name AS 'vendor_name', v.description AS 'vendor_description'
-	FROM [ISYS4283].[dbo].[products] p
-	JOIN [ISYS4283].[dbo].[vendors] v
-	  ON p.vendor = v.id
-
-	-- add this always true condition so that we can simply append with AND
-	WHERE 1 = 1
-";
-
-// if URL query string
-// has a product ID filter
-// and validate user input is an integer
-if(isset($_GET['id']) && is_int(filter_var($_GET['id'], FILTER_VALIDATE_INT))){
-
-	// append SQL with parameterized placeholder
-	$sql .= " AND p.id = :id_parameter";
-
-	// stash value separately for prepared statement
-	$parameters['id_parameter'] = $_GET['id'];
-}
-
-// check URL for vendor filter
-if(isset($_GET['vendor'])){
-
-	// append SQL with parameterized placeholder
-	$sql .= " AND p.vendor = :vendor_parameter";
-
-	// stash value separately for prepared statement
-	$parameters['vendor_parameter'] = $_GET['vendor'];
-}
-
-// if no filters, just query the SELECT statement
-// without a WHERE clause
-if(empty($parameters)){
-	$statement = $db->query($sql);
-}
-// else, bind the user input in a parameterized query
-else {
-	// prepare the statement
-	$statement = $db->prepare($sql);
-
-	// execute the statement
-	// with user submitted data
-	$statement->execute($parameters);
-}
-
-// fetch all rows with associative (non-numeric) indexes
-$result = $statement->fetchAll(PDO::FETCH_ASSOC);
+$products = require __DIR__."/../include/db/products.array.php";
 
 // print out all data in JSON format
-echo json_encode($result, JSON_PRETTY_PRINT);
+echo json_encode($products, JSON_PRETTY_PRINT);
