9d63eb2de471d0b9fd4c21b512dc0134c1c2e09a
Jeff Puckett <jeff@jeffpuckett.com>
2016-11-17 17:15:44 +0000

    add product view

---
+++ include/views/product.php
@@ -0,0 +1,38 @@
+<div class="well row">
+
+	<!-- column 1 -->
+	<div class="col-md-6">
+		<!-- name -->
+		<p class="lead">
+			<?= $name ?>
+		</p>
+
+		<!-- image -->
+		<img class="img-responsive" src="<?= $image ?>"/>
+	</div>
+
+	<!-- column 2 -->
+	<div class="col-md-6">
+		<!-- description -->
+		<p>
+			<?= $description ?>
+		</p>
+
+		<!-- buy now button -->
+		<button data-product="<?= $id ?>" class="btn btn-success">
+			<?= $price ?>
+		</button>
+
+		<hr/>
+
+		<!-- vendor info -->
+		<p><a href="?vendor=<?= $vendor ?>" class="btn btn-default">
+			<?= $vendor_name ?>
+		</a></p>
+		<p>
+			<?= $vendor_description ?>
+		</p>
+	</div>
+
+
+</div>
--- index.php
+++ index.php
@@ -2,4 +2,11 @@
 
 require 'include/header.php';
 
+$products = require 'include/db/products.array.php';
+
+foreach($products as $product){
+	extract($product);
+	require 'include/views/product.php';
+}
+
 require 'include/footer.php';
